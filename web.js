7052a2c146a46e26b9ab6c1b0993ebd7883073f6
federicoig <federicoignacio1@gmail.com>
2020-05-15 16:29:11 -0300

    add more info and new grid

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 const furnitureLink = document.querySelector("#furniture")
-const gallery = document.querySelector("#gallery")
+const gallery = document.querySelector(".separator")
 
 furnitureLink.addEventListener("click", smoothScrollTo =>
     gallery.scrollIntoView({behavior: "smooth"})
 )
 
 const aboutLink = document.querySelector("#about")
-const info = document.querySelector("#info")
+const info = document.querySelector(".separator2")
 
 aboutLink.addEventListener("click", smoothScrollTo =>
     info.scrollIntoView({behavior: "smooth"})
